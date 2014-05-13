b70865a267b7981edd24ae44034621a49a33787b
Prakash Sanker <psanker@stanford.edu>
2014-05-12 21:41:01 -0700

    animated building

--- landscape.js
+++ landscape.js
@@ -10,7 +10,31 @@ globXAngle = 40;
 globYAngle = 40;
 globWidth = 400;
 globHeight = 400;
+	// var canvas = document.getElementById("drawing");
+	// var context = canvas.getContext('2d');
+	// context.beginPath();
+	// context.moveTo(centerX, centerY);
+	// //draw center line
+	// context.lineTo(centerX, centerY - height);
+	// //draw upper right line
+	// var yToDrawTo = (centerY - height) - ((width/2) * getTanDeg(xAngle));
+	// var topFaceHeight = yToDrawTo;
+	// context.lineTo(centerX + width/2, yToDrawTo);
+	// context.lineTo(centerX + width/2, yToDrawTo + height);
+	// context.lineTo(centerX, centerY);
 
+	// yToDrawTo = centerY - ((width/2) * getTanDeg(xAngle));
+	// console.log(yToDrawTo);
+
+	// context.lineTo(centerX - width/2, yToDrawTo);
+	// context.lineTo(centerX - width/2, yToDrawTo - height);
+	// context.lineTo(centerX, centerY - height);
+	// context.moveTo(centerX - width/2, yToDrawTo - height);
+	// var topFaceHeight = getTanDeg(xAngle) * (width/2);
+	// context.lineTo(centerX, centerY  - height -  topFaceHeight * 2);
+	// yToDrawTo = (centerY - height) - ((width/2) * getTanDeg(xAngle));
+	// context.lineTo(centerX + width/2, yToDrawTo);
+	// context.stroke();
 
 function Building(centerX, centerY, xAngle, yAngle, width, height, context) {
 	this.centerX = centerX;
@@ -26,17 +50,40 @@ function Building(centerX, centerY, xAngle, yAngle, width, height, context) {
 	var line2 = new Line(centerX, centerY - height, centerX + width/2, yToDrawTo);
 	var line3 = new Line(centerX + width/2, yToDrawTo, centerX + width/2, yToDrawTo + height);
 	var line4 = new Line(centerX + width/2, yToDrawTo + height, centerX, centerY);
+	yToDrawTo = centerY - ((width/2) * getTanDeg(xAngle));
+	var line5 = new Line(centerX, centerY,centerX - width/2, yToDrawTo);
+	var line6 = new Line(centerX - width/2, yToDrawTo, centerX - width/2, yToDrawTo - height);
+	var line7 = new Line(centerX - width/2, yToDrawTo - height, centerX, centerY - height);
+	var topFaceHeight = getTanDeg(xAngle) * (width/2);
+	var line8 = new Line(centerX - width/2, yToDrawTo - height, centerX, centerY - height - topFaceHeight*2);
+	yToDrawTo = (centerY - height) - ((width/2) * getTanDeg(xAngle));
+	var line9 = new Line(centerX, centerY - height - topFaceHeight*2, centerX + width/2, yToDrawTo);
 
 	this.lines.push(line1);
 	this.lines.push(line2);
 	this.lines.push(line3);
 	this.lines.push(line4);
+	this.lines.push(line5);
+	this.lines.push(line6);
+	this.lines.push(line7);
+	this.lines.push(line8);
+	this.lines.push(line9);
 
 	this.draw = function(context, request) {
-		this.lines[2].draw(this.context);
-		// for(var i = 0; i < this.lines.length; i++){
-		// 	this.lines[i].draw(this.context);
-		// }
+		console.log(" IN THIS> DRAW");
+		console.log(this.lines);
+		console.log("startX");
+		console.log(centerX);
+		console.log("START Y");
+		console.log(centerY);
+		console.log("endX");
+		console.log(centerX);
+		console.log("endY");
+		console.log(centerY - height);
+		// this.lines[0].draw(this.context, request);
+		for(var i = 0; i < this.lines.length; i++){
+			this.lines[i].draw(this.context);
+		}
 	};
 }
 
@@ -49,33 +96,46 @@ function Line (startX, startY, endX, endY) {
 		context.beginPath();
 		console.log("DRAW");
 		context.moveTo(this.startX, this.startY);
-		console.log(this.startX);
-		console.log(this.startY);
-		step += 0.005;
+		step += 0.0005;
 		if (step > 1) step = 1;
+
 		var newX = this.startX + (this.endX - this.startX) * step;
 		var newY = this.startY + (this.endY - this.startY) * step;
+		console.log("NEW X");
+		console.log(newX);
+		console.log("NEW Y");
+		console.log(newY);
+		console.log("CALCULATION");
+		console.log("=====");
 		context.lineTo(newX, newY);
 		context.stroke();
 		if (Math.abs(newX) >= Math.abs(this.endX) && Math.abs(newY) >= Math.abs(this.endY)) {
+			// cancelAnimationFrame(request);
 			//now trigger the next using Backbone Events
-			step = 0;
 		}
 	};
 }
 
 function animateWrapper(centerX, centerY, xAngle, yAngle, width, height) {
-	var line = new Line(centerX, centerY, centerX, centerY - height);
+	var yToDrawTo = (centerY - height) - ((width/2) * getTanDeg(xAngle));
+	console.log("CENTER X");
+	console.log(centerX);
+	console.log("CENTER Y - height");
+	console.log(centerY - height);
+	console.log("CENTER X + WIDTH /2");
+	console.log(centerX + width/2);
+	console.log("Y to draw to");
+	console.log(yToDrawTo);
+	var line = new Line(centerX, centerY - height, centerX + width/2, yToDrawTo);
 	var canvas = document.getElementById("drawing");
 	var context = canvas.getContext('2d');
+
 	var building = new Building(centerX, centerY, xAngle, yAngle, width, height, context);
 	animate();
 
-	console.log(building);
-	console.log(request);
-	function animate() {	var request = requestAnimationFrame(animate);
-
-		console.log("ANIMATE CALLED");
+	function animate() {	
+		// line.draw(context, request);
+		requestAnimationFrame(animate);
 		building.draw();
 	};
 
@@ -132,5 +192,5 @@ function drawBuilding(centerX, centerY, xAngle, yAngle, width, height) {
 
 }
 $(document).ready(function(){
-	animateWrapper(100, 100, 40, 40, 400, 400);
+	animateWrapper(600, 800, 40, 40, 400, 400);
 });
